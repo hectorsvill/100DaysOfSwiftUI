7c0a8b98af0ff2fec077992d3d3f6bc58271914a
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-20 18:17:58 -0700

    form with section

--- Projects/WeSplit/WeSplit/WeSplitApp.swift
+++ Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -15,3 +15,9 @@ struct WeSplitApp: App {
         }
     }
 }
+
+struct WeSplitApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
