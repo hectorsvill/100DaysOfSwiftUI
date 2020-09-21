a4d86eadce0380bc7b8ec117863b844c65825844
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-20 17:58:41 -0700

    Projects folder, WeSplit

---
+++ Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hector Villasano on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
