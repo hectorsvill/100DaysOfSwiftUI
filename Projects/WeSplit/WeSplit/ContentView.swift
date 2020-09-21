89d933cf6f5a0754ca23bbf606d7357b40ef2dd4
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-20 21:12:46 -0700

    Calculating total

--- Projects/WeSplit/WeSplit/ContentView.swift
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 1
+    @State private var tipPercentage = 0
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        
         NavigationView {
             Form {
                 Section {
@@ -28,23 +37,19 @@ struct ContentView: View {
                     }
                 }
                 
-                Section(header: Text("How much tip to leave:")) {
-                    Picker("Tip Percentage: ", selection: $tipPercentage) {
-                        ForEach(0..<self.tipPercentages.count) { _ in
-                            Text("\(tipPercentages[tipPercentage])%")
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
                         }
-                        
-                    }.pickerStyle(SegmentedPickerStyle())
-                    
-                    
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
-                
                 
                 Section {
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
-            
             .navigationTitle("WeSplit")
         }
     }
