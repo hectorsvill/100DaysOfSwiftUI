cf8dd14eb880ebc6311562fcc8242872b201a015
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-24 14:41:57 -0700

    custom modifiers

--- Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -6,36 +6,59 @@
 //
 
 import SwiftUI
+//
+//struct CapsuleText: View {
+//    var text: String
+//
+//    var body: some View {
+//        Text(text)
+//            .font(.largeTitle)
+//            .padding()
+//            .background(Color.blue)
+//            .clipShape(Capsule())
+//    }
+//}
+//struct ContentView: View {
+//
+////    @State private var useGreenText = false
+//
+//    var firstName: some View {
+//        Text("Hector")
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            firstName
+//                .foregroundColor(.green)
+//            CapsuleText(text: "Steven")
+//                .foregroundColor(.white)
+//            CapsuleText(text: "Villasano")
+//        }.font(.largeTitle)    }
+//}
 
-struct CapsuleText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
+            .foregroundColor(.white)
             .padding()
             .background(Color.blue)
-            .clipShape(Capsule())
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
 
 struct ContentView: View {
-    
-//    @State private var useGreenText = false
-    
-    var firstName: some View {
-        Text("Hector")
-    }
-    
     var body: some View {
-        VStack {
-            firstName
-                .foregroundColor(.green)
-            CapsuleText(text: "Steven")
-                .foregroundColor(.white)
-            CapsuleText(text: "Villasano")
-        }.font(.largeTitle)    }
+        Text("Hector")
+            .titleStyle()
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
