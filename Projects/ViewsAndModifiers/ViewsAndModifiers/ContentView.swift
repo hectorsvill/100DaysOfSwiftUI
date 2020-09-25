34ec2c834166254d633f9e3a719fc34e4617c693
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-25 09:35:24 -0700

    wrap up

--- Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -36,27 +36,57 @@ import SwiftUI
 //        }.font(.largeTitle)    }
 //}
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
+//struct Title: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .font(.largeTitle)
+//            .foregroundColor(.white)
+//            .padding()
+//            .background(Color.blue)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//    }
+//}
+//
+//extension View {
+//    func titleStyle() -> some View {
+//        self.modifier(Title())
+//    }
+//}
 
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { colums in
+                        self.content(row, colums)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hector")
-            .titleStyle()
+        GridStack(rows: 4, columns: 4) { row, col  in
+            HStack {
+                Image(systemName: "\(row * 4 + col).circle").font(.title)
+//                Text("R\(row) C\(col)")
+                
+            }
+            
+        }
         
     }
 }
