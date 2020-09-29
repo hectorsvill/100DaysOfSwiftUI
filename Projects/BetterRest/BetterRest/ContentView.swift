b7fe327c54a3adeb33b0e33978389e0173670bac
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-28 23:09:39 -0700

    connecting swiftUI with CoreML

--- Projects/BetterRest/BetterRest/ContentView.swift
+++ Projects/BetterRest/BetterRest/ContentView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -19,6 +24,8 @@ struct ContentView: View {
                     .font(.headline)
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                
+                Text("Desired amount of sleep")
                 Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                     Text("\(sleepAmount, specifier: "%g")")
                 }
@@ -40,13 +47,35 @@ struct ContentView: View {
                 Button(action: calculateBedTime) {
                     Text("Calculate")
                 }
-            
             )
+            
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            })
         }
-        
     }
     
     func calculateBedTime() {
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formater = DateFormatter()
+            formater.timeStyle = .short
+            alertMessage = formater.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is.."
+        } catch {
+            print(error.localizedDescription)
+            alertTitle = "Error"
+            alertMessage = "Error with calculation"
+        }
+        
+        showingAlert = true
         
     }
     
