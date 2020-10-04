0ac133b15baf36ef6a6a43c2d0e077dac8ed3f5f
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-04 16:29:29 -0700

    customizing animations

--- Projects/Animations/Animations/ContentView.swift
+++ Projects/Animations/Animations/ContentView.swift
@@ -12,16 +12,34 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap This") {
-            self.animationAmount += 1
-            print(self)
+            self.animationAmount += 1 
         }.padding(50)
         .background(Color.green)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default)
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation
+                        .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
             
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(
+//            Animation.easeOut(duration: 2)
+//                .repeatForever(autoreverses: true)
+//                .repeatCount(2, autoreverses: true)
+//                .delay(0.5)
+//        ) //.interpolatingSpring(stiffness: 50, damping: 1))
     }
 }
 
