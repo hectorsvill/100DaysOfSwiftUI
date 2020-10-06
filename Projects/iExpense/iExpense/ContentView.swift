17902e11a82fcf80137ba0a04eaa17d6c5e97ccc
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-06 15:49:37 -0700

    showing and presenting views

--- Projects/iExpense/iExpense/ContentView.swift
+++ Projects/iExpense/iExpense/ContentView.swift
@@ -7,12 +7,44 @@
 
 import SwiftUI
 
+
+// Showing and hiding Views
+
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
+    
+    var body: some View {
+        VStack {
+            Text("Hi \(name)")
+            Button("Dismiss") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+        
+    }
+}
+
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "@hectorsvill")
+        }
+    }
+}
+
+// Sharing SwiftUI state with @ObservedObject
 class User: ObservableObject {
     @Published var firstName = "Hector"
     @Published var lastName = "Villa"
 }
 
-struct ContentView: View {
+struct ContentView2: View {
     @ObservedObject private var user = User()
     
     var body: some View {
@@ -24,6 +56,7 @@ struct ContentView: View {
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
