053e3c3ce795ae963a653a8a115cba871f2e3c7b
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-13 18:12:11 -0700

    json files

--- Projects/Moonshot/Moonshot/ContentView.swift
+++ Projects/Moonshot/Moonshot/ContentView.swift
@@ -12,31 +12,6 @@ struct ContentView: View {
     
     var body: some View {
         Button("Decode JSON") {
-            
-            let input = """
-            {
-                    "name": "Taylor Swift",
-                    "address": {
-                        "street": "555, Taylor Swift Avenue",
-                        "city": "Nashville"
-                    }
-                }
-            """
-            
-            struct User: Codable {
-                var name: String
-                var address: Address
-            }
-            
-            struct Address: Codable {
-                var street: String
-                var city: String
-            }
-            
-            let data = Data(input.utf8)
-            if let user = try? JSONDecoder().decode(User.self, from: data) {
-                print(user)
-            }
         }
         
     }
