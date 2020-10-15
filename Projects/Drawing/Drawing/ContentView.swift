ba3160dd11d229b447716bf8cc4575fc2568fc46
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-15 12:50:34 -0700

    Creating custom paths with SwiftUI

---
+++ Projects/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Hector Villasano on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+        .stroke(
+            Color.blue.opacity(0.1),
+            style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
