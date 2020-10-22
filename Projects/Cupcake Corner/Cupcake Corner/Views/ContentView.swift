32039cfd43ef97d741d9b80a3e36ece193d2cef3
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-21 18:33:05 -0700

    Validating and disabling forms

--- Projects/Cupcake Corner/Cupcake Corner/Views/ContentView.swift
+++ Projects/Cupcake Corner/Cupcake Corner/Views/ContentView.swift
@@ -13,9 +13,22 @@ struct ContentView: View {
     
     var body: some View {
         Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
             
+            Section {
+                Button("Create account") {
+                    print("Creating account.. ")
+                }
+            }
+            .disabled(disableForm)
         }
-        
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 }
 
